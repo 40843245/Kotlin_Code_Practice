a149458ab4948a57f0869c85b97f43772c3ac99d
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2024-07-26 15:28:01 +0800

    Update A1.kt

--- Android studio exercise/colab exercise/ch6 - Practice Problems: Kotlin Basics/A1.kt
+++ Android studio exercise/colab exercise/ch6 - Practice Problems: Kotlin Basics/A1.kt
@@ -1,3 +1,7 @@
+/*
+Q at:
+https://developer.android.com/codelabs/basic-android-kotlin-compose-intro-kotlin-practice-problems?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-intro-kotlin-practice-problems#1
+*/
 fun main(){
   println("Use the val keyword when the value doesn't change.")
   println("Use the var keyword when the value can change.")

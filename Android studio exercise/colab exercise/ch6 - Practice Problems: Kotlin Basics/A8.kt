fff0cbdf6780a53b81bd45db4e0fe5a4ea3ee64a
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2024-07-26 16:26:21 +0800

    Create A8.kt

---
+++ Android studio exercise/colab exercise/ch6 - Practice Problems: Kotlin Basics/A8.kt
@@ -0,0 +1,25 @@
+/*
+Q at:
+https://developer.android.com/codelabs/basic-android-kotlin-compose-intro-kotlin-practice-problems?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-intro-kotlin-practice-problems#8
+*/
+
+/*
+1.Can you rename the functions, function parameters, and variables in this program based on best practices?
+Yes.
+
+The best practice of function name and variable name both follow camelCase.
+
+The following code is:
+*/
+
+fun main() {
+    val steps = 4000
+    val caloriesBurned = pedometerStepsToCalories(steps);
+    println("Walking $steps steps burns $caloriesBurned calories") 
+}
+
+fun pedometerStepsToCalories(numberOfSteps: Int): Double {
+    val caloriesBurnedForEachStep = 0.04
+    val totalCaloriesBurned = numberOfSteps * caloriesBurnedForEachStep
+    return totalCaloriesBurned
+}
